675985a575b313f2facb2245a96300597ac9fac3
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-22 14:54:30 +0000

    adding routes to app-routing.module.ts

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router'
+
+const routes: Routes = [
+  {
+    path: 'private', 
+    loadChildren: () => import('./private/private.module').then(m => m.PrivateModule)
+  },
+  {
+    path: 'public',
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'public',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
