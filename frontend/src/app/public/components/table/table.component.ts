0b14b2db8567a8a1a158d79bf5237f10d9c7ee03
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-23 15:08:28 +0000

    improved table css

--- frontend/src/app/public/components/table/table.component.ts
+++ frontend/src/app/public/components/table/table.component.ts
@@ -3,13 +3,13 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-interface PeriodicElement {
+interface CryptoCoins {
   name: string,
   price: number,
   mc: number,
   dc: number
  }
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: CryptoCoins[] = [
   {
     name: "BTC",
     price: 16591,
@@ -62,70 +62,3 @@ export class TableComponent implements AfterViewInit {
     }
   }
 }
-
-
-// import {LiveAnnouncer} from '@angular/cdk/a11y';
-// import {AfterViewInit, Component, ViewChild} from '@angular/core';
-// import {MatSort, Sort} from '@angular/material/sort';
-
-// interface Crypto {
-//   name: string,
-//    price: string,
-//    mc: string,
-//    dc: string
-//  }
-
-// @Component({
-//   selector: 'table-root',
-//   templateUrl: './table.component.html',
-//   styleUrls: ['./table.component.css']
-// })
-
-// export class TableComponent implements AfterViewInit {
-
-  //  crypto: Crypto[] = [
-  //    {
-  //      "name": "BTC",
-  //      "price": "16591",
-  //      "mc": "318881782391",
-  //      "dc": "+5%"
-  //    },
-  //    {
-  //      "name": "ETH",
-  //      "price": "1172",
-  //      "mc": "141297194997",
-  //      "dc": "+7.3%"
-  //    },
-  //    {
-  //      "name": "USDT",
-  //      "price": "0.99",
-  //      "mc": "65483976330",
-  //      "dc": "-0.2%"
-  //    },
-  //    {
-  //      "name": "BNB",
-  //      "price": "297",
-  //      "mc": "48437062392",
-  //      "dc": "+16.4%"
-  //    }
-  //  ]
-  
-//   displayedColumns: string[] = ['name', 'price', 'mc', 'dc'];
-//   dataSource: any = this.crypto
-
-//   constructor(private _liveAnnouncer: LiveAnnouncer) {}
-
-//   @ViewChild(MatSort) sort!: MatSort;
-
-//   ngAfterViewInit() {
-//     this.dataSource.sort = this.sort;
-//   }
-
-//   announceSortChange(sortState: Sort) {
-//     if (sortState.direction) {
-//       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-//     } else {
-//       this._liveAnnouncer.announce('Sorting cleared');
-//     }
-//   }
-// }
