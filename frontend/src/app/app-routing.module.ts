e3c1d1658d6e3012be806bc4a621d7d9ba8e30d0
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-23 09:26:06 +0000

    dashboard redirects to login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router'
+import { AuthGuard } from './guards/auth.guard'
 
 const routes: Routes = [
   {
     path: 'private', 
+    canActivate: [AuthGuard],
     loadChildren: () => import('./private/private.module').then(m => m.PrivateModule)
   },
   {
