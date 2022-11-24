3ca4f7c8a73e82ae292eb9a2e0aeed040c6a8f0d
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-24 15:42:03 +0000

    global data api pulling to frontend

--- api/src/global/global.controller.ts
+++ api/src/global/global.controller.ts
@@ -6,8 +6,8 @@ export class GlobalController {
   constructor(private readonly globalService: GlobalService) {}
 
   @Get()
-  async get(): Promise<{ data: any }> {
-    let data = await this.globalService.getData();
-    return { data };
+  async get(): Promise<{ globalData: any }> {
+    let globalData = await this.globalService.getData();
+    return { globalData };
   }
 }
