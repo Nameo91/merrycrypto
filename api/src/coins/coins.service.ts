c3801a779d73f8056f97b98372581386a74fc42f
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-24 12:16:40 +0000

    table sorting correctly

--- api/src/coins/coins.service.ts
+++ api/src/coins/coins.service.ts
@@ -29,11 +29,12 @@ export class CoinService {
         imageURL: any
        }
 
+
       function specificData(coin: any) {
         const coinObject: CryptoCoin = {
           name: coin.CoinInfo.Name,
-          price: coin.DISPLAY.USD.PRICE,
-          mc: coin.DISPLAY.USD.MKTCAP,
+          price: Number(coin.DISPLAY.USD.PRICE.replace(/[^0-9.-]+/g,"")),
+          mc: Number(coin.DISPLAY.USD.MKTCAP.replace(/[^0-9.-]+/g,"")),
           dc: coin.DISPLAY.USD.CHANGEPCT24HOUR,
           imageURL: coin.DISPLAY.USD.IMAGEURL
         }
