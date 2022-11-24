eb7bf7ea666668937d19dd07830735744dab4d1a
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-24 18:16:08 +0000

    news api linking to frontend

--- frontend/src/app/public/components/news/news.component.ts
+++ frontend/src/app/public/components/news/news.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/news.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit{
+   news!: any;
 
+   constructor(private newsService: NewsService) {}
+
+   ngOnInit(): void {
+     this.getNews()
+   }
+
+   getNews() {
+    this.newsService.getNews().subscribe(news => {
+      this.news = news
+    })
+   }
+
+   get loading(): boolean {
+    return this.news === undefined;
+  }
 }
