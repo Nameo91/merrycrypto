5db997583338f02579c27727a1d422a5fbd1940e
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-25 14:55:15 +0000

    basic search bar working

--- frontend/src/app/public/components/search/search.component.ts
+++ frontend/src/app/public/components/search/search.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
 
+export class SearchComponent {
+  cryptoName: string = "";
+
+  constructor(private router: Router) {}
+
+  getSearchResult(cryptoName: any) {
+    this.cryptoName = cryptoName.target.value;
+    console.log('/coins/'.concat(this.cryptoName));
+    this.navigateTo('/coins/'.concat(this.cryptoName))
+  }
+
+  navigateTo(value: string) {
+    this.router.navigate([value]);
+  }
 }
