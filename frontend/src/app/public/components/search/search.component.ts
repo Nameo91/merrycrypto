aa4a5f339ddf5eee73014a47716b77e95595ee51
Na-Rae Kim <narae1096@gmail.com>
2022-11-28 09:46:50 +0000

    correct coinservice path for search component

--- frontend/src/app/public/components/search/search.component.ts
+++ frontend/src/app/public/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CoinsService } from '../../../../app/coins.service'
+import { CoinsService } from 'src/app/services/coins.service'
 
 
 @Component({
