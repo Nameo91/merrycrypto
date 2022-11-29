6287fb48a70b5b460d05f26f2fea0c7fdf552847
Lauren Brabbin <laurenbrabbin@MASTUDENT-Lauren-Brabbin-C17G44KUQ6L4.local>
2022-11-29 09:19:00 +0000

    total portfolio stats now pulling in from database

--- frontend/src/app/public/components/portfolio-table/portfolio-table.component.ts
+++ frontend/src/app/public/components/portfolio-table/portfolio-table.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
 import { PriceService } from 'src/app/price.service';
-import { PortfolioService } from 'src/app/services/portfolio.service';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
  interface CryptoCoins {
@@ -11,39 +9,6 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   amountBought: number,
   [key: string]: any
  }
-
-const PortfolioData: CryptoCoins[] = [
-//   {
-//     name: "BTC",
-//     priceBought: 10591,
-//     amountBought: 5,
-//     imgURL: "/media/37746251/btc.png"
-//   },
-//   {
-//     name: "ETH",
-//     priceBought: 2200,
-//     amountBought: 12,
-//     imgURL: "/media/37746238/eth.png"
-//   },
-//   {
-//     name: "DOGE",
-//     priceBought: 0.08,
-//     amountBought: 1000,
-//     imgURL: "/media/37746339/doge.png"
-//   },
-//   {
-//     name: "BNB",
-//     priceBought: 400,
-//     amountBought: 200,
-//     imgURL: "/media/40485170/bnb.png"
-//   },
-//   {
-//     name: "USDT",
-//     priceBought: 0.99,
-//     amountBought: 35000,
-//     imgURL: "/media/37746338/usdt.png"
-//   }
-];
 
 @Component({
   selector: 'app-portfolio-table',
@@ -56,7 +21,7 @@ export class PortfolioTableComponent implements OnInit {
   dataSource: any
   numFor = Intl.NumberFormat('en-US')
 
-  constructor(private priceService: PriceService, private portfolioService: PortfolioService, private authService: AuthenticationService,) {}
+  constructor(private priceService: PriceService, private authService: AuthenticationService,) {}
   
   ngOnInit(): void {
     this.authService.getUserInfo().subscribe((data) => {
@@ -76,9 +41,4 @@ export class PortfolioTableComponent implements OnInit {
    }) 
   }
 }
-
-// element['price'] = price
-// element['holdings'] = this.numFor.format((price * element.amountBought))
-// element['pnl'] = ((price * element.amountBought) - (element.priceBought * element.amountBought))
-// element['percentagePNL'] = (((price * element.amountBought) - (element.priceBought * element.amountBought)) / (element.priceBought * element.amountBought) * 100).toFixed(0)
 
