1e78e9920b99282f6d73ca6ab1b4decfd73eaf09
Na-Rae Kim <narae1096@gmail.com>
2022-11-30 09:53:52 +0000

    add new type of tweets

--- frontend/src/app/public/components/related-tweet/related-tweet.component.ts
+++ frontend/src/app/public/components/related-tweet/related-tweet.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { RelatedTweetService } from 'src/app/services/related-tweet.service';
 import { ActivatedRoute } from '@angular/router';
 
+type Tweets = {
+  created_at: string;
+  text: string;
+  author_id: string;
+  user: {
+    name: string;
+    id: string;
+    profile_image_url: string;
+    username: string;
+  }
+}[];
+
 @Component({
   selector: 'app-related-tweet',
   templateUrl: './related-tweet.component.html',
   styleUrls: ['./related-tweet.component.css']
 })
+
 export class RelatedTweetComponent implements OnInit {
   public id!: any;
-  relatedTweets!: any;
+  relatedTweets: Tweets=[];
   showTweets: boolean = false;
   buttonName: 'Show' | 'Hide' = 'Show';
 
@@ -24,9 +37,8 @@ export class RelatedTweetComponent implements OnInit {
   }
 
   getRelatedTweet(id: string) {
-    this.relatedTweetService.getRelatedTweets(id).subscribe((tweets: any) => {
+    this.relatedTweetService.getRelatedTweets(id).subscribe((tweets: Tweets) => {
       this.relatedTweets = tweets;
-      console.log(tweets)
     })
   }
 
